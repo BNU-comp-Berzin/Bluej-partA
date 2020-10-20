6ecb5b00775ef093052c8dcae518800827b47629
Berzin-2K <72166114+Berzin-2K@users.noreply.github.com>
2020-10-20 15:18:53 +0100

    app02 - ticket machine
    Printed  train ticket, with price and destination, as well as the name of the railway line. This has been done with Aylesbury so far

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+
+/** This class is includes all the price, destiantion and date details on the ticket
+ * 
+ *
+ * @author (Berzin Daruwala)
+ * @version (18/10/20)
+ */
+public class Ticket
+{// instance variables - replace the example below with your own
+    public String destination;
+    
+    private int price;
+    
+    private Date datePurchased = new Date(); 
+    
+
+    /**
+     * Constructor for objects of class Ticket
+     */
+    
+    public Ticket()
+    {
+        price = 220;
+        destination = "Aylesbury";
+        datePurchased = new Date();
+    }
+    
+    
+    
+    public Ticket(String destination, int price)
+    {
+        // initialise instance variable
+        this.price = price;
+        this.destination = destination;
+        datePurchased = new Date();
+        
+    }
+    
+    public String getDestination()
+    {
+        return destination;
+    }
+    public int getPrice()
+    {
+        return price;
+    }
+    public void print()
+    { 
+        System.out.print("Destination:" + destination);
+        System.out.print(" Price " + price + " pence");
+    }
+}
--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -9,7 +9,7 @@
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  * 
- * Modified by Student Name
+ * Modified by Berzin Daruwala
  */
 public class TicketMachine
 {
@@ -19,17 +19,25 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
-
+    
+    private Ticket ticketToAylesbury;
+    
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine()
     {
-        price = cost;
-        balance = 0;
         total = 0;
+        balance = 0;
+        createTickets();
     }
-
+    /**
+     * creates a ticket with it's destination and price included
+     */
+    private void createTickets()
+    {
+       ticketToAylesbury = new Ticket("Aylesbury", 220);
+    }
     /**
      * @Return The price of a ticket.
      */
@@ -74,12 +82,7 @@ public class TicketMachine
         if(balance >= price) 
         {
             // Simulate the printing of a ticket.
-            System.out.println("##################");
-            System.out.println("# The BlueJ Line");
-            System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
-            System.out.println("##################");
-            System.out.println();
+            
 
             // Update the total collected with the price.
             total = total + price;
@@ -105,4 +108,23 @@ public class TicketMachine
         balance = 0;
         return amountToRefund;
     }
+    
+    public void printAllTickets()
+    {
+        printHeading();
+        System.out.println();
+        ticketToAylesbury.print();
+    }
+    
+    /**
+     * Prints out the name of the ticket with the hashtag borders
+     */
+    public void printHeading()
+    {
+        System.out.println("##################");
+        System.out.println("# The BlueJ Line");
+        System.out.println("##################");
+        System.out.println();
+    }
 }
+
