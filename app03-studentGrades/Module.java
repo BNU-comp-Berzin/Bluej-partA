a26104e430e7210f8c87157719c45e86ed9737de
Berzin-2K <72166114+Berzin-2K@users.noreply.github.com>
2020-10-28 12:07:25 +0000

    added methods and contructors
    constructors and print methods added to module class

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -2,32 +2,32 @@
 /**
  * Write a description of class Module here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Berzin Daruwala)
+ * @version (28/10/2020)
  */
 public class Module
 {
+    private String title;
+    private String codeNo;
+    private int mark;
+    
+    
+   
     // instance variables - replace the example below with your own
-    private int x;
 
     /**
      * Constructor for objects of class Module
      */
-    public Module()
+    public Module(String title, String codeNo)
     {
-        // initialise instance variables
-        x = 0;
+       mark = 0;
+       this.title = title;
+       this.codeNo = codeNo;
+    }
+    
+    public void print()
+    {
+        System.out.println("Module: " + codeNo + " " + title + " Mark = " + mark);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
